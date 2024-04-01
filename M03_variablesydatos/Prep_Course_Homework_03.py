7f3e66c44b30e7eb469b61bb580f473317114f93
Tatiana Gomez <gerika@unicauca.edu.co>
2024-04-01 11:14:19 -0500

    cambio_homework

--- M03_variablesydatos/Prep_Course_Homework_03.py
+++ M03_variablesydatos/Prep_Course_Homework_03.py
@@ -3,174 +3,110 @@
 
 # ## Variables
 
-# 1) Crear una variable que contenga un elemento del conjunto de números enteros y luego imprimir por pantalla
+'''1) Crear una variable que contenga un elemento del conjunto de números enteros y
+luego imprimir por pantalla
+'''
 
-# In[7]:
-
-
-
+numero = 4
+print(numero)
 
 # 2) Imprimir el tipo de dato de la constante 8.5
-
-# In[3]:
-
-
-
-
+numero2 = 8.5
+print(type(numero2))
 
 # 3) Imprimir el tipo de dato de la variable creada en el punto 1
-
-# In[8]:
-
-
-
-
+print(type(numero))
 
 # 4) Crear una variable que contenga tu nombre
-
-# In[2]:
-
-
-
+nombre = 'Tatiana'
+print(nombre)
 
 # 5) Crear una variable que contenga un número complejo
-
-# In[3]:
-
-
-
-
+numero_complejo = 5 + 4j
 
 # 6) Mostrar el tipo de dato de la variable crada en el punto 5
-
-# In[4]:
-
-
-
-
+print(type(numero_complejo))
 
 # 7) Crear una variable que contenga el valor del número Pi redondeado a 4 decimales
-
-# In[1]:
-
-
 pi = 3.1416
 
 
 # 8) Crear una variable que contenga el valor 'True' y otra que contenga el valor True. ¿Se trata de lo mismo?
+cadena = 'True'
 
-# In[3]:
-
-
-
+logica = True
 
 
 # 9) Imprimir el tipo de dato correspondientes a las variables creadas en el punto 8
-
-# In[5]:
-
-
-
-
+print(type(cadena))
+print(type(logica))
 
 # 10) Asignar a una variable, la suma de un número entero y otro decimal
 
-# In[1]:
+numero_entero = 34
+numero_decimal = 24.654
 
-
-
-
+print(numero_entero + int(numero_decimal))
 
 # 11) Realizar una operación de suma de números complejos
+numero_1 = 4 +5j
+numero_2 = 5 + 3j
 
-# In[2]:
-
-
-
-
+suma = numero_1 + numero_2
+print(suma)
 
 # 12) Realizar una operación de suma de un número real y otro complejo
+numero_real = 45
+numero_complejo = 3 +5j
 
-# In[4]:
-
-
-
-
+suma = numero_real + numero_complejo
+print(suma)
 
 # 13) Realizar una operación de multiplicación
-
-# In[5]:
-
-
-
-
+multiplicacion = 6 * 4
+print(multiplicacion)
 
 # 14) Mostrar el resultado de elevar 2 a la octava potencia
-
-# In[6]:
-
-
-
+potencia = 2 ** 8
+print(potencia)
 
 # 15) Obtener el cociente de la división de 27 entre 4 en una variable y luego mostrarla
-
-# In[8]:
-
-
-
-
+cociente = 27/4
+print(cociente)
 
 # 16) De la división anterior solamente mostrar la parte entera
-
-# In[9]:
-
-
-
-
+print(int(cociente))
 
 # 17) De la división de 27 entre 4 mostrar solamente el resto
-
-# In[1]:
-
-
-
-
+resto = 27 % 4
+print(resto)
 
 # 18) Utilizando como operandos el número 4 y los resultados obtenidos en los puntos 16 y 17. Obtener 27 como resultado
 
-# In[2]:
-
-
-
-
+operacion = 6 * 4 + 3
+print(operacion)
 
 # 19) Utilizar el operador "+" en una operación donde intervengan solo variables alfanuméricas
 
-# In[3]:
+variable_1 = 'holi'
+variable_2 = 'mundo'
 
-
-
-
+concatenacion = variable_1 + variable_2
+print(concatenacion)
 
 # 20) Evaluar si "2" es igual a 2. ¿Por qué ocurre eso?
 
-# In[4]:
-
-
-
-
+igualdad = '2' == 2
+print(igualdad)
 
 # 21) Utilizar las funciones de cambio de tipo de dato, para que la validación del punto 20 resulte verdadera
 
-# In[11]:
-
-
-
-
+nueva_igualdad = 2 == 2
+print(nueva_igualdad)
 
 # 22) ¿Por qué arroja error el siguiente cambio de tipo de datos? a = float('3,8')
 
-# In[12]:
+# porque solo se puede pasar a flotantes numero no strings
 
 
 
@@ -178,32 +114,31 @@ pi = 3.1416
 
 # 23) Crear una variable con el valor 3, y utilizar el operador '-=' para modificar su contenido y que de como resultado 2.
 
-# In[15]:
-
-
-
-
+variable_3 = 3
+variable_3 = variable_3 - 1
+print(variable_3)
 
 # 24) Realizar la operacion 1 << 2 ¿Por qué da ese resultado? ¿Qué es el sistema de numeración binario?
 
-# In[29]:
+bitss = 1 << 2
+print(bitss)
 
-
-
+# da 4 porque se desplazan 2 espacios hacia la izquierda y los espacios vacios quedan 0
 
 
 # 25) Realizar la operación 2 + '2' ¿Por qué no está permitido? ¿Si los dos operandos serían del mismo tipo, siempre arrojaría el mismo resultado?
 
-# In[23]:
-
-
-
-
-
+# da error porque no se pueden sumar numeros con strings
 
 # 26) Realizar una operación válida entre valores de tipo entero y string
 
-# In[30]:
+concatenacion = 34 + int('43')
+print(concatenacion)
+
+
+
+
+
 
 
 
--- M04_flujosdecontrol/Prep_Course_Homework_04.py
+++ M04_flujosdecontrol/Prep_Course_Homework_04.py
@@ -4,107 +4,96 @@
 # ## Flujos de Control
 
 # 1) Crear una variable que contenga un elemento del conjunto de números enteros y luego imprimir por pantalla si es mayor o menor a cero
+numero = 6
 
-# In[4]:
-
-
-
-
+if numero > 0:
+    print('ES mayor a 0')
+else:
+    print('Es menor a 0 ')
 
 # 2) Crear dos variables y un condicional que informe si son del mismo tipo de dato
 
-# In[5]:
-
-
-
-
+variable_1 = 5
+variable_2 = 4
+if type(variable_1) == type(variable_2):
+    print('Son del mismo tipo')
+else:
+    print('Son diferentes')
 
 # 3) Para los valores enteros del 1 al 20, imprimir por pantalla si es par o impar
-
-# In[7]:
-
-
-
-
+for i in range(1,20):
+    if i % 2 == 0:
+        print('Es par')
+    else:
+        print('Es impar')
 
 # 4) En un ciclo for mostrar para los valores entre 0 y 5 el resultado de elevarlo a la potencia igual a 3
 
-# In[9]:
-
-
-
+for i in range(0,5):
+    print(i**3)
 
 
 # 5) Crear una variable que contenga un número entero y realizar un ciclo for la misma cantidad de ciclos
 
-# In[10]:
+variable_3 = 6
 
-
-
-
+for i in range(0, variable_3):
+    print(i)
 
 # 6) Utilizar un ciclo while para realizar el factoreo de un número guardado en una variable, sólo si la variable contiene un número entero mayor a 0
-
-# In[33]:
-
-
-
-
+numero = 4
+while numero > 0:
+     cuadrado = numero **2
+     numero = numero - 1
+     print(cuadrado)
 
 # 7) Crear un ciclo for dentro de un ciclo while
 
-# In[38]:
-
-
-
-
+n = 0
+while(n < 5):
+    n += 1
+    for i in range(1,n):
+        print(i)
 
 # 8) Crear un ciclo while dentro de un ciclo for
 
-# In[3]:
-
+n = 5
+for i in range(1, n):
+    while(n < 5):
+        n -= 1
+        print('Ciclo while nro ' + str(n))
+        print('Ciclo for nro ' + str(i))
 
 
 
 
 # 9) Imprimir los números primos existentes entre 0 y 30
-
-# In[54]:
-
-
-
+num = 31
+for i in range(0,num):
+    if i % num == 0:
+        print('No es primo')
+    else:
+        print(i)
 
 # 10) ¿Se puede mejorar el proceso del punto 9? Utilizar las sentencias break y/ó continue para tal fin
 
-# In[55]:
-
-
-
-
+num = 31
+for i in range(0,num):
+    if i % num == 1:
+        print(i)
+    break
 
 # 11) En los puntos 9 y 10, se diseño un código que encuentra números primos y además se lo optimizó. ¿Es posible saber en qué medida se optimizó?
-
-# In[56]:
-
-
-
-
-# In[57]:
-
 
 
 
 # 12) Aplicando continue, armar un ciclo while que solo imprima los valores divisibles por 12, dentro del rango de números de 100 a 300
 
-# In[62]:
-
-
-
 
 
 # 13) Utilizar la función **input()** que permite hacer ingresos por teclado, para encontrar números primos y dar la opción al usario de buscar el siguiente
 
-# In[73]:
+
 
 
 
@@ -112,6 +101,5 @@
 # 14) Crear un ciclo while que encuentre dentro del rango de 100 a 300 el primer número divisible por 3 y además múltiplo de 6
 
 # In[75]:
-
-
+#22
 
